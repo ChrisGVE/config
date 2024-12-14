6a98169355ef9be0c8143eb9b78d20c51d1c881f
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-14 10:32:36 +0100

    chore(brew): automatic update of the homebrew lists

--- homebrew/extract-brew.sh
+++ homebrew/extract-brew.sh
@@ -6,3 +6,9 @@
 /usr/local/bin/brew leaves >~/.config/homebrew/brew-leaves.txt
 /usr/local/bin/brew list --cask >~/.config/homebrew/brew-casks.txt
 /usr/local/bin/brew list --formula >~/.config/homebrew/brew-formulas.txt
+
+# Update git repo with these changes
+cd ~/.config/homebrew || exit
+git add ./*glob*
+git commit -am "chore(brew): automatic update of the homebrew lists"
+git push
