7e1805eea0eb99eb0824e3c6aa1fed64d42dddc0
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-19 21:36:12 +0100

    chore(brew): automatic update of the homebrew lists

--- homebrew/extract-brew.sh
+++ homebrew/extract-brew.sh
@@ -3,7 +3,7 @@
 # Extracts the list installed brew formulae/casks to a file
 #
 
-/usr/local/bin/brew bundle dump --all --describe --quiet --file ~/.config/homebrew/Brewfile
+/usr/local/bin/brew bundle dump --all --describe --quiet --force --file ~/.config/homebrew/Brewfile
 /usr/local/bin/brew leaves >~/.config/homebrew/brew-leaves.txt
 /usr/local/bin/brew list --cask >~/.config/homebrew/brew-casks.txt
 /usr/local/bin/brew list --formula >~/.config/homebrew/brew-formulas.txt
