2ee5ad29b5a64b1ae3df07c4ab127f9fc19f27bf
Christian C. Berclaz <christian.berclaz@mac.com>
2025-01-11 09:41:47 +0100

    feat(fish): Added a fish configuration

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /usr/local/Caskroom/miniconda/base/bin/conda
+    eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/usr/local/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/usr/local/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/usr/local/Caskroom/miniconda/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
